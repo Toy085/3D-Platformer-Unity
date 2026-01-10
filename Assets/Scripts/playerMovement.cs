8041715599ea65f762548dbbe11e181de30da90c
Toy085 <brightabaka1@gmail.com>
2026-01-10 10:39:22 +0100

    Added fade animation to death

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -2,22 +2,27 @@ using System;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class playerMovement : MonoBehaviour
 {
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector3 playerVelocity;
+    private Vector3 lastCheckpointPos;
     private bool groundedPlayer;
     private bool jumpPressed;
 
     public float speed = 5f;
     public float jumpPower = 2f;
     public float gravity = -9.81f;
+    public float deathY = -5f;
     public Animator animator;
 
     private int coins = 0;
     public HUDUI hudUI;
+    public CanvasGroup fadeGroup;
+    public float fadeDuration = 1f;
 
     public CinemachineCamera freeLookCamera;
     private void Start()
@@ -26,6 +31,7 @@ public class playerMovement : MonoBehaviour
         GetComponent<PlayerInput>().SwitchCurrentActionMap("Player");
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        lastCheckpointPos = transform.position;
     }
     private void OnMove(InputValue value)
     {
@@ -84,6 +90,11 @@ public class playerMovement : MonoBehaviour
         {
             animator.SetBool("IsJumping", false);
         }
+
+        if (this.transform.position.y <= deathY)
+        {
+            StartCoroutine(Respawn());
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -94,6 +105,36 @@ public class playerMovement : MonoBehaviour
             coins++;
             hudUI.setCoinUI(coins);
         }
+    }
+
+    public void SetCheckpoint(Vector3 pos)
+    {
+        lastCheckpointPos = pos;
+    }
+    IEnumerator Respawn()
+    {
+        yield return StartCoroutine(Fade(1));
+
+        controller.enabled = false;
+        transform.position = lastCheckpointPos;
+        playerVelocity = Vector3.zero;
+        controller.enabled = true;
+        yield return StartCoroutine(Fade(1));
+        yield return StartCoroutine(Fade(0));
+    }
+    IEnumerator Fade(float targetAlpha)
+    {
+        float startAlpha = fadeGroup.alpha;
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            fadeGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, time / fadeDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeGroup.alpha = targetAlpha;
     }
 
 }
