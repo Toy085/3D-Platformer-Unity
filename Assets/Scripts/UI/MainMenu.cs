65cd154d0b8817dc24e6f743457bdb5e1bf7d0ea
Toy085 <brightabaka1@gmail.com>
2026-01-10 19:25:25 +0100

    Added settings menu to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,11 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("LevelSelect"); // Change to save menu later
     }
-
     public void QuitGame()
     {
         Application.Quit();
