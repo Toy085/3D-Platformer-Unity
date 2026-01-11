2274049a3604d5a5011f6c24287a433090dfc258
Toy085 <brightabaka1@gmail.com>
2026-01-11 09:05:45 +0100

    Added pause menu

--- Assets/Scripts/UI/HUDUI.cs
+++ Assets/Scripts/UI/HUDUI.cs
@@ -1,12 +1,56 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class HUDUI : MonoBehaviour
 {
     public TextMeshProUGUI coinText;
 
+    public GameObject pausePanel;
+    private bool isPaused = false;
+    public PlayerInput playerInput;
+
     public void setCoinUI(int amount)
     {
         coinText.text = "Coins: " + amount.ToString();
+    }
+
+    public void OnPause(InputValue value)
+    {
+        if (!value.isPressed) return;
+
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        playerInput.SwitchCurrentActionMap("UI");
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        playerInput.SwitchCurrentActionMap("Player");
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
     }
 }
