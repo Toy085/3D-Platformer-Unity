07da7bd37f3f4602ea365c6abfc3249f9e426551
Toy085 <brightabaka1@gmail.com>
2026-01-13 18:38:22 +0100

    Added Level5 to level Select

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerData
 {
-    public int coins;
-    public float maxHealth;
-    public Vector3 playerPosition;
-    public int levelsCompleted;
-    public int checkpointSceneIndex;
+    public int coins = 0;
+    public float maxHealth = 100f;
+    public Vector3 playerPosition = Vector3.zero;
+    public int levelsCompleted = 0;
+    public int checkpointSceneIndex = -1;
 }
