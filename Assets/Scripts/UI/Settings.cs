abd3cd3c139670286257f61ead50d8353206ac3c
Toy085 <brightabaka1@gmail.com>
2026-01-19 18:43:20 +0100

    Added Music volume slider to settings menu

--- Assets/Scripts/UI/Settings.cs
+++ Assets/Scripts/UI/Settings.cs
@@ -6,6 +6,7 @@ public class Settings : MonoBehaviour
     public GameObject settingsPanel;
     public Toggle fullscreenToggle;
     public Slider volumeSlider;
+    public Slider musicVolumeSlider;
     public Slider sensitivitySlider;
 
     void Start()
@@ -13,11 +14,20 @@ public class Settings : MonoBehaviour
         AudioListener.volume = PlayerPrefs.GetFloat("Volume", 1f);
         volumeSlider.value = AudioListener.volume;
 
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        musicVolumeSlider.value = musicVolume;
+        MusicManager.Instance?.SetMusicVolume(musicVolume);
+
         bool isFullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
         Screen.fullScreen = isFullscreen;
         if (fullscreenToggle != null)
         {
             fullscreenToggle.isOn = isFullscreen;
+        }
+
+        if (sensitivitySlider != null)
+        {
+            sensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity", 1f);
         }
     }
     public void OpenSettings()
@@ -42,5 +52,10 @@ public class Settings : MonoBehaviour
     public void SetSensitivity(float value)
     {
         PlayerPrefs.SetFloat("Sensitivity", value);
+    }
+    public void SetMusicVolume(float value)
+    {
+        MusicManager.Instance?.SetMusicVolume(value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
     }
 }
